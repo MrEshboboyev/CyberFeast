4ea75ecd29a475f821cd9e8215d32b5566ea6ee9
mreshboboyev <mreshboboyev@gmail.com>
2025-01-10 12:14:00 +0500

    feat: Add detailed comments and summaries to ServiceActivator class
    - Added detailed comments and summaries to `ServiceActivator`, explaining its purpose and usage for interacting with the IServiceProvider.
    - Documented the `Configure` method for configuring the service activator with a specified service provider.
    - Documented the `GetScope` method for creating a new service scope using the specified or configured service provider.
    - Enhanced the `GetService` and `GetRequiredService` methods with detailed comments on retrieving services from the configured service provider.
    - Provided clear documentation for methods to retrieve single and multiple services by type from the DI container.
    
    This commit improves code readability and maintainability by providing clear documentation and comments for the `ServiceActivator` class used to interact with the IServiceProvider in various application scenarios.

---
+++ src/BuildingBlocks/BuildingBlocks.Core/ServiceActivator.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Core;
+
+/// <summary>
+/// Provides static methods to interact with the IServiceProvider.
+/// </summary>
+public static class ServiceActivator
+{
+    /// <summary>
+    /// The service provider instance.
+    /// </summary>
+    internal static IServiceProvider? _serviceProvider;
+
+    /// <summary>
+    /// Configures the service activator with the specified service provider.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider to configure.</param>
+    public static void Configure(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    /// <summary>
+    /// Creates a new service scope using the specified or configured service provider.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider to use. If null, uses the configured service provider.</param>
+    /// <returns>A new service scope.</returns>
+    public static IServiceScope? GetScope(IServiceProvider? serviceProvider = null)
+    {
+        var provider = serviceProvider ?? _serviceProvider;
+        return provider?.GetRequiredService<IServiceScopeFactory>().CreateScope();
+    }
+
+    /// <summary>
+    /// Gets a service of the specified type from the configured service provider.
+    /// </summary>
+    /// <typeparam name="T">The type of the service to retrieve.</typeparam>
+    /// <returns>An instance of the specified service type.</returns>
+    public static T? GetService<T>()
+    {
+        return _serviceProvider.GetService<T>();
+    }
+
+    /// <summary>
+    /// Gets a required service of the specified type from the configured service provider.
+    /// </summary>
+    /// <typeparam name="T">The type of the service to retrieve.</typeparam>
+    /// <returns>An instance of the specified service type.</returns>
+    public static T GetRequiredService<T>()
+    {
+        return _serviceProvider.GetRequiredService<T>();
+    }
+
+    /// <summary>
+    /// Gets a required service of the specified type from the configured service provider.
+    /// </summary>
+    /// <param name="type">The type of the service to retrieve.</param>
+    /// <returns>An instance of the specified service type.</returns>
+    public static object GetRequiredServices(Type type)
+    {
+        return _serviceProvider.GetRequiredService(type);
+    }
+
+    /// <summary>
+    /// Gets all services of the specified type from the configured service provider.
+    /// </summary>
+    /// <typeparam name="T">The type of the services to retrieve.</typeparam>
+    /// <returns>An enumerable collection of the specified service type.</returns>
+    public static IEnumerable<T> GetServices<T>()
+    {
+        return _serviceProvider.GetServices<T>();
+    }
+
+    /// <summary>
+    /// Gets a service of the specified type from the configured service provider.
+    /// </summary>
+    /// <param name="type">The type of the service to retrieve.</param>
+    /// <returns>An instance of the specified service type.</returns>
+    public static object GetService(Type type)
+    {
+        return _serviceProvider.GetService(type);
+    }
+
+    /// <summary>
+    /// Gets all services of the specified type from the configured service provider.
+    /// </summary>
+    /// <param name="type">The type of the services to retrieve.</param>
+    /// <returns>An enumerable collection of the specified service type.</returns>
+    public static IEnumerable<object> GetServices(Type type)
+    {
+        return _serviceProvider.GetServices(type);
+    }
+}
