691826601075dde8e438e34c2953c8115ed4a87a
mreshboboyev <mreshboboyev@gmail.com>
2025-01-11 17:51:28 +0500

    feat: Add detailed comments and summaries to PostgreSQL EF Core persistence classes and configurations
    - DbContextDesignFactoryBase: Added detailed comments and summaries to `DbContextDesignFactoryBase`, explaining its purpose and usage for creating DbContext instances during design-time. Documented methods for setting up the configuration builder and constructing DbContext options with PostgreSQL-specific settings.
    - AddPostgresDbContext: Documented `AddPostgresDbContext` method with clear summaries and comments on adding and configuring PostgreSQL DbContext in the dependency injection container. Provided detailed documentation on private methods for adding PostgreSQL repositories and unit of work to the service collection.
    - Configuration and Assemblies Handling: Explained the configurations and assemblies handling for migrations and scanning, including optional parameter usage.
    
    This commit improves code readability and maintainability by providing clear documentation and comments for PostgreSQL EF Core persistence classes and configurations used in applications.

---
+++ src/BuildingBlocks/BuildingBlocks.Persistence.EfCore.Postgres/DbContextDesignFactoryBase.cs
@@ -0,0 +1,59 @@
+﻿using BuildingBlocks.Core.Persistence.EFCore;
+using BuildingBlocks.Core.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Configuration;
+
+namespace BuildingBlocks.Persistence.EfCore.Postgres;
+
+/// <summary>
+/// Provides a base class for creating design-time DbContext instances.
+/// </summary>
+/// <typeparam name="TDbContext">The type of DbContext.</typeparam>
+public abstract class DbContextDesignFactoryBase<TDbContext>(
+    string connectionStringSection,
+    string? env = null) : IDesignTimeDbContextFactory<TDbContext> where TDbContext : DbContext
+{
+    /// <summary>
+    /// Creates a new instance of the DbContext for design-time operations.
+    /// </summary>
+    /// <param name="args">Arguments passed by the tools.</param>
+    /// <returns>A new instance of the DbContext.</returns>
+    public TDbContext CreateDbContext(string[] args)
+    {
+        Console.WriteLine($"BaseDirectory: {AppContext.BaseDirectory}");
+
+        var environmentName = env ?? Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? Environments.Test;
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{environmentName}.json", true) // optional
+            .AddEnvironmentVariables();
+
+        var configuration = builder.Build();
+        var connectionStringSectionValue = configuration.GetValue<string>(connectionStringSection);
+
+        if (string.IsNullOrWhiteSpace(connectionStringSectionValue))
+        {
+            throw new InvalidOperationException($"Could not find a value for {connectionStringSection} section.");
+        }
+
+        Console.WriteLine($"ConnectionString section value is: {connectionStringSectionValue}");
+
+        var optionsBuilder = new DbContextOptionsBuilder<TDbContext>()
+            .UseNpgsql(
+                connectionStringSectionValue,
+                sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(GetType().Assembly.FullName);
+                    sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(30), null);
+                }
+            )
+            .UseSnakeCaseNamingConvention()
+            .ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector<long>>();
+
+        return (TDbContext)Activator.CreateInstance(typeof(TDbContext), optionsBuilder.Options);
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Persistence.EfCore.Postgres/DependencyInjectionExtensions.cs
@@ -0,0 +1,114 @@
+﻿using System.Reflection;
+using BuildingBlocks.Abstractions.Events;
+using BuildingBlocks.Abstractions.Persistence;
+using BuildingBlocks.Abstractions.Persistence.EFCore;
+using BuildingBlocks.Core.Extensions;
+using BuildingBlocks.Core.Extensions.ServiceCollection;
+using BuildingBlocks.Core.Persistence.EFCore;
+using BuildingBlocks.Core.Persistence.EFCore.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace BuildingBlocks.Persistence.EfCore.Postgres;
+
+/// <summary>
+/// Provides extension methods for registering and configuring PostgreSQL DbContext instances.
+/// </summary>
+public static class DependencyInjectionExtensions
+{
+    /// <summary>
+    /// Adds and configures a PostgreSQL DbContext in the service collection.
+    /// </summary>
+    /// <typeparam name="TDbContext">The type of DbContext.</typeparam>
+    /// <param name="services">The service collection.</param>
+    /// <param name="configuration">The configuration instance.</param>
+    /// <param name="migrationAssembly">The migration assembly. Optional.</param>
+    /// <param name="builder">An optional action to further configure the DbContext options.</param>
+    /// <param name="configurator">An optional action to configure the Postgres options.</param>
+    /// <param name="assembliesToScan">Assemblies to scan for repositories and unit of work patterns.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddPostgresDbContext<TDbContext>(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        Assembly? migrationAssembly = null,
+        Action<DbContextOptionsBuilder>? builder = null,
+        Action<PostgresOptions>? configurator = null,
+        params Assembly[] assembliesToScan
+    )
+        where TDbContext : DbContext, IDbFacadeResolver, IDomainEventContext
+    {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        // Add options to the dependency injection
+        services.AddValidationOptions(configurator);
+        var options = configuration.BindOptions(configurator);
+
+        var connStr = options.ConnectionString!.NotBeEmptyOrNull();
+        services.TryAddScoped<IConnectionFactory>(_ => new NpgsqlConnectionFactory(connStr));
+
+        services.AddDbContext<TDbContext>((sp, dbContextOptionsBuilder) =>
+        {
+            dbContextOptionsBuilder
+                .UseNpgsql(connStr, sqlOptions =>
+                {
+                    var name = migrationAssembly?.GetName().Name ??
+                               options.MigrationAssembly ?? typeof(TDbContext).Assembly.GetName().Name;
+                    sqlOptions.MigrationsAssembly(name);
+                    sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+                })
+                .UseSnakeCaseNamingConvention()
+                .ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector<long>>();
+
+            dbContextOptionsBuilder.AddInterceptors(
+                new AuditInterceptor(),
+                new SoftDeleteInterceptor(),
+                new ConcurrencyInterceptor()
+            );
+
+            builder?.Invoke(dbContextOptionsBuilder);
+        });
+
+        services.TryAddScoped<IDbFacadeResolver>(provider => provider.GetService<TDbContext>()!);
+        services.TryAddScoped<IDomainEventContext>(provider => provider.GetService<TDbContext>()!);
+
+        services.AddPostgresRepositories(assembliesToScan);
+        services.AddPostgresUnitOfWork(assembliesToScan);
+
+        return services;
+    }
+
+    private static IServiceCollection AddPostgresRepositories(
+        this IServiceCollection services, params Assembly[] assembliesToScan)
+    {
+        var scanAssemblies = assembliesToScan.Length != 0 ? assembliesToScan : new[] { Assembly.GetCallingAssembly() };
+
+        services.Scan(scan =>
+            scan.FromAssemblies(scanAssemblies)
+                .AddClasses(classes => classes.AssignableTo(typeof(IRepository<,>)), false)
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .WithTransientLifetime()
+        );
+
+        return services;
+    }
+
+    private static IServiceCollection AddPostgresUnitOfWork(
+        this IServiceCollection services, params Assembly[] assembliesToScan)
+    {
+        var scanAssemblies = assembliesToScan.Length != 0 ? assembliesToScan : new[] { Assembly.GetCallingAssembly() };
+
+        services.Scan(scan =>
+            scan.FromAssemblies(scanAssemblies)
+                .AddClasses(classes => classes.AssignableTo(typeof(IEFUnitOfWork<>)), false)
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .WithTransientLifetime()
+        );
+
+        return services;
+    }
+}
