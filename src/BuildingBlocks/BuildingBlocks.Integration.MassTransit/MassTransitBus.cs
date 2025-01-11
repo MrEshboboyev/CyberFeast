42d6397008a6d42f77336d5451205084d9aad5cb
mreshboboyev <mreshboboyev@gmail.com>
2025-01-11 14:48:44 +0500

    feat: Add detailed comments and summaries to MassTransit integration classes
    - Added detailed comments and summaries to `MassTransitBus`, explaining its purpose and usage for message publishing and consuming using MassTransit.
    - Documented methods for publishing messages and event envelopes, both generically and to specific exchanges or topics and queues.
    - Enhanced `MasstransitDirectPublisher` with thorough documentation for its direct message publishing capabilities using MassTransit.
    - Provided detailed comments on methods for filling MassTransit context information and constructing endpoint addresses.
    
    This commit improves code readability and maintainability by providing clear documentation for MassTransit integration classes used for message handling in applications.

---
+++ src/BuildingBlocks/BuildingBlocks.Integration.MassTransit/MassTransitBus.cs
@@ -0,0 +1,204 @@
+﻿using BuildingBlocks.Abstractions.Events;
+using BuildingBlocks.Abstractions.Messaging;
+using BuildingBlocks.Abstractions.Messaging.PersistMessage;
+using BuildingBlocks.Core.Events;
+using BuildingBlocks.Core.Messaging;
+using Humanizer;
+using Microsoft.Extensions.Options;
+using MessageHeaders = BuildingBlocks.Core.Messaging.MessageHeaders;
+
+namespace BuildingBlocks.Integration.MassTransit;
+
+/// <summary>
+/// An implementation of the IExternalEventBus interface using MassTransit for message publishing and consuming.
+/// </summary>
+public class MassTransitBus(
+    IBusDirectPublisher busDirectPublisher,
+    IMessageMetadataAccessor messageMetadataAccessor,
+    IMessagePersistenceService messagePersistenceService,
+    IOptions<MessagingOptions> messagingOptions
+) : IExternalEventBus
+{
+    private readonly MessagingOptions _messagingOptions = messagingOptions.Value;
+
+    /// <summary>
+    /// Publishes a message asynchronously.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message to publish.</typeparam>
+    /// <param name="message">The message to publish.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken = default)
+        where TMessage : IMessage
+    {
+        var correlationId = messageMetadataAccessor.GetCorrelationId();
+        var cautionId = messageMetadataAccessor.GetCorrelationId();
+        var messageTypeName = message.GetType().Name.Underscore();
+
+        var eventEnvelope = EventEnvelope.From(
+            message,
+            correlationId,
+            cautionId,
+            new Dictionary<string, object?>
+            {
+                { MessageHeaders.ExchangeOrTopic, $"{messageTypeName}{MessagingConstants.PrimaryExchangePostfix}" },
+                { MessageHeaders.Queue, messageTypeName }
+            }
+        );
+
+        return PublishAsync(eventEnvelope, cancellationToken);
+    }
+
+    /// <summary>
+    /// Publishes an event envelope asynchronously.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message contained in the envelope.</typeparam>
+    /// <param name="eventEnvelope">The event envelope to publish.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task PublishAsync<TMessage>(
+        IEventEnvelope<TMessage> eventEnvelope,
+        CancellationToken cancellationToken = default
+    )
+        where TMessage : IMessage
+    {
+        if (_messagingOptions.OutboxEnabled)
+        {
+            await messagePersistenceService.AddPublishMessageAsync(eventEnvelope, cancellationToken);
+            return;
+        }
+
+        await busDirectPublisher.PublishAsync(eventEnvelope, cancellationToken);
+    }
+
+    /// <summary>
+    /// Publishes a message to a specific exchange or topic and queue asynchronously.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message to publish.</typeparam>
+    /// <param name="message">The message to publish.</param>
+    /// <param name="exchangeOrTopic">The exchange or topic to publish to.</param>
+    /// <param name="queue">The queue to publish to.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task PublishAsync<TMessage>(
+        TMessage message,
+        string? exchangeOrTopic = null,
+        string? queue = null,
+        CancellationToken cancellationToken = default
+    )
+        where TMessage : IMessage
+    {
+        var correlationId = messageMetadataAccessor.GetCorrelationId();
+        var cautionId = messageMetadataAccessor.GetCorrelationId();
+        var messageTypeName = message.GetType().Name.Underscore();
+
+        var eventEnvelope = EventEnvelope.From(
+            message,
+            correlationId,
+            cautionId,
+            new Dictionary<string, object?>
+            {
+                {
+                    MessageHeaders.ExchangeOrTopic,
+                    exchangeOrTopic ?? $"{messageTypeName}{MessagingConstants.PrimaryExchangePostfix}"
+                },
+                { MessageHeaders.Queue, queue ?? messageTypeName }
+            }
+        );
+
+        if (_messagingOptions.OutboxEnabled)
+        {
+            await messagePersistenceService.AddPublishMessageAsync(eventEnvelope, cancellationToken);
+            return;
+        }
+
+        await busDirectPublisher.PublishAsync(eventEnvelope, exchangeOrTopic, queue, cancellationToken);
+    }
+
+    /// <summary>
+    /// Publishes an event envelope to a specific exchange or topic and queue asynchronously.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message contained in the envelope.</typeparam>
+    /// <param name="eventEnvelope">The event envelope to publish.</param>
+    /// <param name="exchangeOrTopic">The exchange or topic to publish to.</param>
+    /// <param name="queue">The queue to publish to.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task PublishAsync<TMessage>(
+        IEventEnvelope<TMessage> eventEnvelope,
+        string? exchangeOrTopic = null,
+        string? queue = null,
+        CancellationToken cancellationToken = default
+    )
+        where TMessage : IMessage
+    {
+        var messageTypeName = eventEnvelope.Message.GetType().Name.Underscore();
+
+        if (_messagingOptions.OutboxEnabled)
+        {
+            await messagePersistenceService.AddPublishMessageAsync(eventEnvelope, cancellationToken);
+            return;
+        }
+
+        await busDirectPublisher.PublishAsync(
+            eventEnvelope,
+            exchangeOrTopic ?? $"{messageTypeName}{MessagingConstants.PrimaryExchangePostfix}",
+            queue ?? messageTypeName,
+            cancellationToken
+        );
+    }
+
+    public void Consume<TMessage>(
+        IMessageHandler<TMessage> handler,
+        Action<IConsumeConfigurationBuilder>? consumeBuilder = null
+    )
+        where TMessage : IMessage
+    {
+    }
+
+    public Task Consume<TMessage>(
+        Abstractions.Messaging.MessageHandler<TMessage> subscribeMethod,
+        Action<IConsumeConfigurationBuilder>? consumeBuilder = null,
+        CancellationToken cancellationToken = default
+    )
+        where TMessage : IMessage
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task Consume<TMessage>(CancellationToken cancellationToken = default)
+        where TMessage : IMessage
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task Consume(Type messageType, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task Consume<THandler, TMessage>(CancellationToken cancellationToken = default)
+        where THandler : IMessageHandler<TMessage>
+        where TMessage : IMessage
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task ConsumeAll(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task ConsumeAllFromAssemblyOf<TType>(CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task ConsumeAllFromAssemblyOf(
+        CancellationToken cancellationToken = default,
+        params Type[] assemblyMarkerTypes
+    )
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Integration.MassTransit/MasstransitDirectPublisher.cs
@@ -0,0 +1,205 @@
+﻿using BuildingBlocks.Abstractions.Events;
+using BuildingBlocks.Abstractions.Messaging;
+using BuildingBlocks.Core.Messaging;
+using Humanizer;
+using MassTransit;
+using RabbitMQ.Client;
+
+namespace BuildingBlocks.Integration.MassTransit;
+
+/// <summary>
+/// An implementation of the IBusDirectPublisher interface using MassTransit for direct message publishing.
+/// </summary>
+public class MasstransitDirectPublisher(IBus bus) : IBusDirectPublisher
+{
+    /// <summary>
+    /// Publishes an event envelope asynchronously.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message contained in the envelope.</typeparam>
+    /// <param name="eventEnvelope">The event envelope to publish.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task PublishAsync<TMessage>(
+        IEventEnvelope<TMessage> eventEnvelope,
+        CancellationToken cancellationToken = default
+    )
+        where TMessage : IMessage
+    {
+        await bus.Publish(
+            eventEnvelope,
+            envelopeWrapperContext =>
+                FillMasstransitContextInformation(eventEnvelope, envelopeWrapperContext),
+            cancellationToken
+        );
+    }
+
+    /// <summary>
+    /// Publishes a non-generic event envelope asynchronously.
+    /// </summary>
+    /// <param name="eventEnvelope">The event envelope to publish.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public Task PublishAsync(IEventEnvelope eventEnvelope, CancellationToken cancellationToken = default)
+    {
+        var messageType = eventEnvelope.Message.GetType();
+        var publishMethod = typeof(IBusDirectPublisher)
+            .GetMethods()
+            .FirstOrDefault(x => 
+                x.GetGenericArguments().Length != 0 && x.GetParameters().Length == 2)!;
+        var genericPublishMethod = publishMethod.MakeGenericMethod(messageType);
+        var publishTask = (Task)genericPublishMethod
+            .Invoke(this, [eventEnvelope, cancellationToken])!;
+
+        return publishTask!;
+    }
+
+    /// <summary>
+    /// Publishes an event envelope to a specific exchange or topic and queue asynchronously.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message contained in the envelope.</typeparam>
+    /// <param name="eventEnvelope">The event envelope to publish.</param>
+    /// <param name="exchangeOrTopic">The exchange or topic to publish to.</param>
+    /// <param name="queue">The queue to publish to.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task PublishAsync<TMessage>(
+        IEventEnvelope<TMessage> eventEnvelope,
+        string? exchangeOrTopic = null,
+        string? queue = null,
+        CancellationToken cancellationToken = default
+    )
+        where TMessage : IMessage
+    {
+        var bindExchangeName = eventEnvelope.Message.GetType().Name.Underscore();
+
+        if (string.IsNullOrEmpty(exchangeOrTopic))
+        {
+            exchangeOrTopic = $"{bindExchangeName}{MessagingConstants.PrimaryExchangePostfix}";
+        }
+
+        var endpointAddress = GetEndpointAddress(
+            exchangeOrTopic: exchangeOrTopic,
+            queue: queue,
+            bindExchange: bindExchangeName,
+            exchangeType: ExchangeType.Direct
+        );
+
+        var sendEndpoint = await bus.GetSendEndpoint(new Uri(endpointAddress));
+        await sendEndpoint.Send(
+            eventEnvelope,
+            envelopeWrapperContext =>
+                FillMasstransitContextInformation(eventEnvelope, envelopeWrapperContext),
+            cancellationToken
+        );
+    }
+
+    /// <summary>
+    /// Publishes a non-generic event envelope to a specific exchange or topic and queue asynchronously.
+    /// </summary>
+    /// <param name="eventEnvelope">The event envelope to publish.</param>
+    /// <param name="exchangeOrTopic">The exchange or topic to publish to.</param>
+    /// <param name="queue">The queue to publish to.</param>
+    /// <param name="cancellationToken">A token to cancel the operation.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public Task PublishAsync(
+        IEventEnvelope eventEnvelope,
+        string? exchangeOrTopic = null,
+        string? queue = null,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var messageType = eventEnvelope.Message.GetType();
+        var publishMethod = typeof(IBusDirectPublisher)
+            .GetMethods()
+            .FirstOrDefault(x => 
+                x.GetGenericArguments().Length != 0 && x.GetParameters().Length == 4)!;
+        var genericPublishMethod = publishMethod.MakeGenericMethod(messageType);
+        var publishTask = (Task)
+            genericPublishMethod.Invoke(
+                this,
+                [eventEnvelope, exchangeOrTopic, queue, cancellationToken]
+            )!;
+
+        return publishTask!;
+    }
+
+    #region Private Methods
+    
+    /// <summary>
+    /// Fills the MassTransit context information for publishing the event envelope.
+    /// </summary>
+    /// <param name="eventEnvelope">The event envelope.</param>
+    /// <param name="envelopeWrapperContext">The MassTransit publish context.</param>
+    private static void FillMasstransitContextInformation(
+        IEventEnvelope eventEnvelope,
+        PublishContext<IEventEnvelope> envelopeWrapperContext
+    )
+    {
+        envelopeWrapperContext.MessageId = eventEnvelope.Metadata.MessageId;
+        envelopeWrapperContext.CorrelationId = eventEnvelope.Metadata.CorrelationId;
+        envelopeWrapperContext.SetRoutingKey(eventEnvelope.Message.GetType().Name.Underscore());
+
+        foreach (var header in eventEnvelope.Metadata.Headers)
+        {
+            envelopeWrapperContext.Headers.Set(header.Key, header.Value);
+        }
+    }
+
+    /// <summary>
+    /// Fills the MassTransit context information for sending the event envelope.
+    /// </summary>
+    /// <param name="eventEnvelope">The event envelope.</param>
+    /// <param name="envelopeWrapperContext">The MassTransit send context.</param>
+    private static void FillMasstransitContextInformation(
+        IEventEnvelope eventEnvelope,
+        SendContext<IEventEnvelope> envelopeWrapperContext
+    )
+    {
+        envelopeWrapperContext.MessageId = eventEnvelope.Metadata.MessageId;
+        envelopeWrapperContext.CorrelationId = eventEnvelope.Metadata.CorrelationId;
+
+        foreach (var header in eventEnvelope.Metadata.Headers)
+        {
+            envelopeWrapperContext.Headers.Set(header.Key, header.Value);
+        }
+    }
+
+    /// <summary>
+    /// Constructs the endpoint address for publishing or sending the event envelope.
+    /// </summary>
+    /// <param name="exchangeOrTopic">The exchange or topic.</param>
+    /// <param name="queue">The queue.</param>
+    /// <param name="bindExchange">The bind exchange.</param>
+    /// <param name="exchangeType">The exchange type. Defaults to "direct".</param>
+    /// <param name="bindQueue">Indicates whether to bind the queue. Defaults to false.</param>
+    /// <returns>The constructed endpoint address.</returns>
+    private static string GetEndpointAddress(
+        string exchangeOrTopic,
+        string? queue,
+        string? bindExchange,
+        string exchangeType = ExchangeType.Direct,
+        bool bindQueue = false
+    )
+    {
+        var endpoint = $"exchange:{exchangeOrTopic}?type={exchangeType}&durable=true";
+
+        if (!string.IsNullOrEmpty(bindExchange))
+        {
+            endpoint += $"&bindexchange={bindExchange}";
+        }
+
+        if (!string.IsNullOrEmpty(queue))
+        {
+            endpoint += $"&queue={queue}";
+        }
+
+        if (bindQueue)
+        {
+            endpoint += "&bind=true";
+        }
+
+        return endpoint;
+    }
+    
+    #endregion
+}
