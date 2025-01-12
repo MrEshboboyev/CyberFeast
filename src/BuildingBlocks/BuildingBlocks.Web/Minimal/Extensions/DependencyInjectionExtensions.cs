1489c60edace68564f34f48f80aee05b4285acd5
mreshboboyev <mreshboboyev@gmail.com>
2025-01-12 13:24:57 +0500

    feat: Add support for mapping command and query endpoints with minimal APIs
    - Implement EndpointRouteBuilderExtensions class to provide extension methods for mapping command and query endpoints:
      - MapCommandEndpoint<TRequest, TCommand>: Maps a POST endpoint for handling a command with a request and command.
      - MapCommandEndpoint<TRequest, TResponse, TCommand, TCommandResult>: Maps a POST endpoint for handling a command with a request, response, command, and command result.
      - MapQueryEndpoint<TRequestParameters, TResponse, TQuery, TQueryResult>: Maps a GET endpoint for handling a query with request parameters, response, query, and query result.
    - Implement DependencyInjectionExtensions class to provide extension methods for registering minimal endpoints and mapping them:
      - AddMinimalEndpoints (WebApplicationBuilder): Registers minimal endpoints from specified assemblies in the service collection for a web application builder.
      - AddMinimalEndpoints (IServiceCollection): Registers minimal endpoints from specified assemblies in the service collection.
      - MapMinimalEndpoints: Maps the registered minimal endpoints in the endpoint routing, organizing them by version and prefix route.
    
    These changes enhance the handling and mapping of command and query endpoints in minimal APIs, ensuring consistent endpoint registration and routing.

---
+++ src/BuildingBlocks/BuildingBlocks.Web/Minimal/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System.Reflection;
+using BuildingBlocks.Abstractions.Web.MinimalApi;
+using BuildingBlocks.Core.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+
+namespace BuildingBlocks.Web.Minimal.Extensions;
+
+/// <summary>
+/// Provides extension methods for registering minimal endpoints and mapping them in the endpoint routing.
+/// </summary>
+public static class DependencyInjectionExtensions
+{
+    /// <summary>
+    /// Registers minimal endpoints from the specified assemblies in the service collection.
+    /// </summary>
+    /// <param name="applicationBuilder">The web application builder.</param>
+    /// <param name="scanAssemblies">The assemblies to scan for endpoints.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddMinimalEndpoints(
+        this WebApplicationBuilder applicationBuilder,
+        params Assembly[] scanAssemblies)
+    {
+        var assemblies = LoadAssemblies(scanAssemblies);
+
+        applicationBuilder.Services.Scan(scan =>
+            scan.FromAssemblies(assemblies)
+                .AddClasses(classes => classes.AssignableTo<IMinimalEndpoint>())
+                .UsingRegistrationStrategy(RegistrationStrategy.Append)
+                .As<IMinimalEndpoint>()
+                .WithLifetime(ServiceLifetime.Scoped)
+        );
+
+        return applicationBuilder.Services;
+    }
+
+    /// <summary>
+    /// Registers minimal endpoints from the specified assemblies in the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="scanAssemblies">The assemblies to scan for endpoints.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddMinimalEndpoints(
+        this IServiceCollection services,
+        params Assembly[] scanAssemblies)
+    {
+        var assemblies = LoadAssemblies(scanAssemblies);
+
+        services.Scan(scan =>
+            scan.FromAssemblies(assemblies)
+                .AddClasses(classes => classes.AssignableTo<IMinimalEndpoint>())
+                .UsingRegistrationStrategy(RegistrationStrategy.Append)
+                .As<IMinimalEndpoint>()
+                .WithLifetime(ServiceLifetime.Scoped)
+        );
+
+        return services;
+    }
+
+    /// <summary>
+    /// Maps the registered minimal endpoints in the endpoint routing.
+    /// </summary>
+    /// <param name="builder">The endpoint route builder.</param>
+    /// <returns>The updated endpoint route builder.</returns>
+    public static IEndpointRouteBuilder MapMinimalEndpoints(this IEndpointRouteBuilder builder)
+    {
+        var scope = builder.ServiceProvider.CreateScope();
+
+        var endpoints = scope.ServiceProvider.GetServices<IMinimalEndpoint>().ToList();
+
+        var versionGroups = endpoints
+            .GroupBy(x => x.GroupName)
+            .ToDictionary(x => x.Key, c => builder.NewVersionedApi(c.Key).WithTags(c.Key));
+
+        var versionSubGroups = endpoints
+            .GroupBy(x => new { x.GroupName, x.PrefixRoute, x.Version })
+            .ToDictionary(
+                x => x.Key,
+                c => versionGroups[c.Key.GroupName].MapGroup(c.Key.PrefixRoute).HasApiVersion(c.Key.Version)
+            );
+
+        var endpointVersions = endpoints
+            .GroupBy(x => new { x.GroupName, x.Version })
+            .Select(x => new { x.Key.Version, x.Key.GroupName, Endpoints = x.Select(v => v) });
+
+        foreach (var endpointVersion in endpointVersions)
+        {
+            var versionGroup = versionSubGroups
+                .FirstOrDefault(x => x.Key.GroupName == endpointVersion.GroupName)
+                .Value;
+
+            foreach (var ep in endpointVersion.Endpoints)
+            {
+                ep.MapEndpoint(versionGroup);
+            }
+        }
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Loads the specified assemblies or the calling assembly if no assemblies are specified.
+    /// </summary>
+    /// <param name="scanAssemblies">The assemblies to scan.</param>
+    /// <returns>An array of assemblies to scan.</returns>
+    private static Assembly[] LoadAssemblies(Assembly[] scanAssemblies)
+    {
+        return scanAssemblies.Length != 0
+            ? scanAssemblies
+            : ReflectionUtilities.GetReferencedAssemblies(Assembly.GetCallingAssembly())
+                .Concat(ReflectionUtilities.GetApplicationPartAssemblies(Assembly.GetCallingAssembly()))
+                .Distinct()
+                .ToArray();
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Web/Minimal/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,163 @@
+﻿using BuildingBlocks.Abstractions.Commands;
+using BuildingBlocks.Abstractions.Queries;
+using BuildingBlocks.Abstractions.Web.MinimalApi;
+using Humanizer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Routing;
+
+namespace BuildingBlocks.Web.Minimal.Extensions;
+
+/// <summary>
+/// Provides extension methods for mapping command and query endpoints for minimal APIs.
+/// </summary>
+public static class EndpointRouteBuilderExtensions
+{
+    /// <summary>
+    /// Maps a POST endpoint for handling a command with a request and command.
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the request.</typeparam>
+    /// <typeparam name="TCommand">The type of the command.</typeparam>
+    /// <param name="builder">The endpoint route builder.</param>
+    /// <param name="pattern">The URL pattern for the endpoint.</param>
+    /// <param name="mapRequestToCommand">Optional function to map the request to a command.</param>
+    /// <returns>The configured route handler builder.</returns>
+    public static RouteHandlerBuilder MapCommandEndpoint<TRequest, TCommand>(
+        this IEndpointRouteBuilder builder,
+        string pattern,
+        Func<TRequest, TCommand>? mapRequestToCommand = null)
+        where TRequest : class
+        where TCommand : ICommand
+    {
+        // Map a POST endpoint and configure its metadata
+        return builder.MapPost(pattern, Handle)
+            .WithName(typeof(TCommand).Name)
+            .WithDisplayName(typeof(TCommand).Name.Humanize())
+            .WithSummaryAndDescription(
+                typeof(TCommand).Name.Humanize(), typeof(TCommand).Name.Humanize());
+
+        // Handle the command asynchronously
+        async Task<NoContent> Handle([AsParameters] HttpCommand<TRequest> requestParameters)
+        {
+            var (request, _, commandBus, mapper, cancellationToken) = requestParameters;
+
+            var command = mapRequestToCommand is not null
+                ? mapRequestToCommand(request)
+                : mapper.Map<TCommand>(request);
+
+            await commandBus.SendAsync(command, cancellationToken);
+
+            return TypedResults.NoContent();
+        }
+    }
+
+    /// <summary>
+    /// Maps a POST endpoint for handling a command with a request, response, command, and command result.
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the request.</typeparam>
+    /// <typeparam name="TResponse">The type of the response.</typeparam>
+    /// <typeparam name="TCommand">The type of the command.</typeparam>
+    /// <typeparam name="TCommandResult">The type of the command result.</typeparam>
+    /// <param name="builder">The endpoint route builder.</param>
+    /// <param name="pattern">The URL pattern for the endpoint.</param>
+    /// <param name="statusCode">The status code for the response.</param>
+    /// <param name="mapRequestToCommand">Optional function to map the request to a command.</param>
+    /// <param name="mapCommandResultToResponse">Optional function to map the command result to a response.</param>
+    /// <param name="getId">Optional function to get the ID from the response.</param>
+    /// <returns>The configured route handler builder.</returns>
+    public static RouteHandlerBuilder MapCommandEndpoint<
+        TRequest, TResponse, TCommand, TCommandResult>(
+        this IEndpointRouteBuilder builder,
+        string pattern,
+        int statusCode,
+        Func<TRequest, TCommand>? mapRequestToCommand = null,
+        Func<TCommandResult, TResponse>? mapCommandResultToResponse = null,
+        Func<TResponse, long>? getId = null)
+        where TRequest : class
+        where TResponse : class
+        where TCommand : ICommand<TCommandResult>
+        where TCommandResult : class
+    {
+        // Map a POST endpoint and configure its metadata
+        return builder.MapPost(pattern, Handle)
+            .WithName(typeof(TCommand).Name)
+            .WithDisplayName(typeof(TCommand).Name.Humanize())
+            .WithSummaryAndDescription(
+                typeof(TCommand).Name.Humanize(), typeof(TCommand).Name.Humanize());
+
+        // Handle the command asynchronously and return the appropriate response
+        async Task<IResult> Handle([AsParameters] HttpCommand<TRequest> requestParameters)
+        {
+            var (request, context, commandBus, mapper, cancellationToken) = requestParameters;
+            var host = $"{context.Request.Scheme}://{context.Request.Host}";
+
+            var command = mapRequestToCommand is not null
+                ? mapRequestToCommand(request)
+                : mapper.Map<TCommand>(request);
+
+            var result = await commandBus.SendAsync(command, cancellationToken);
+
+            var response = mapCommandResultToResponse is not null
+                ? mapCommandResultToResponse(result)
+                : mapper.Map<TResponse>(result);
+
+            return statusCode switch
+            {
+                StatusCodes.Status201Created => getId is not null
+                    ? TypedResults.Created($"{host}{pattern}/{getId(response)}", response)
+                    : TypedResults.Ok(response),
+                StatusCodes.Status401Unauthorized => TypedResultsExtensions.UnAuthorizedProblem(),
+                StatusCodes.Status500InternalServerError => TypedResultsExtensions.InternalProblem(),
+                StatusCodes.Status202Accepted => TypedResults.Accepted(new Uri($"{host}{pattern}"), response),
+                _ => TypedResults.Ok(response)
+            };
+        }
+    }
+
+    /// <summary>
+    /// Maps a GET endpoint for handling a query with request parameters, response, query, and query result.
+    /// </summary>
+    /// <typeparam name="TRequestParameters">The type of the request parameters.</typeparam>
+    /// <typeparam name="TResponse">The type of the response.</typeparam>
+    /// <typeparam name="TQuery">The type of the query.</typeparam>
+    /// <typeparam name="TQueryResult">The type of the query result.</typeparam>
+    /// <param name="builder">The endpoint route builder.</param>
+    /// <param name="pattern">The URL pattern for the endpoint.</param>
+    /// <param name="mapRequestToQuery">Function to map the request parameters to the query.</param>
+    /// <param name="mapQueryResultToResponse">Function to map the query result to the response.</param>
+    /// <returns>The configured route handler builder.</returns>
+    public static RouteHandlerBuilder MapQueryEndpoint<
+        TRequestParameters, TResponse, TQuery, TQueryResult>(
+        this IEndpointRouteBuilder builder,
+        string pattern,
+        Func<TRequestParameters, TQuery> mapRequestToQuery,
+        Func<TQueryResult, TResponse> mapQueryResultToResponse)
+        where TRequestParameters : IHttpQuery
+        where TResponse : class
+        where TQuery : IQuery<TQueryResult>
+        where TQueryResult : class
+    {
+        // Map a GET endpoint and configure its metadata
+        return builder.MapGet(pattern, Handle)
+            .WithName(typeof(TQuery).Name)
+            .WithDisplayName(typeof(TQuery).Name.Humanize())
+            .WithSummaryAndDescription(
+                typeof(TQuery).Name.Humanize(), typeof(TQuery).Name.Humanize());
+
+        // Handle the query asynchronously and return the appropriate response
+        async Task<Ok<TResponse>> Handle([AsParameters] TRequestParameters requestParameters)
+        {
+            var queryProcessor = requestParameters.QueryBus;
+            var cancellationToken = requestParameters.CancellationToken;
+
+            var query = mapRequestToQuery(requestParameters);
+
+            var result = await queryProcessor.SendAsync(query, cancellationToken);
+
+            var response = mapQueryResultToResponse(result);
+
+            return TypedResults.Ok(response);
+        }
+    }
+}
